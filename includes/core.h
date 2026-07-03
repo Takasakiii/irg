4206519fe22cc30c970ca881fb9568999bfc5650
Takasakiii <lucasmc2709@live.com>
2026-07-03 06:36:54 -0300

    adicionado tipos universais em core.h

---
+++ includes/core.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef struct {
+    const int x;
+    const int y;
+} Coords2D;
+
+typedef struct {
+    const int x;
+    const int y;
+    const int z;
+} Coords3D;
+
+
+typedef Coords2D Size2D;
+
+typedef Coords3D Size3D;
