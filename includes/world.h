a6a734a219a0ee7a3e7feaaf59f45da26b0745fe
Takasakiii <lucasmc2709@live.com>
2026-07-03 06:38:49 -0300

    adicionado types para Chunk, e portado a logica de iterar

--- includes/world.h
+++ includes/world.h
@@ -2,4 +2,22 @@
 
 #include <stdbool.h>
 
+#include "core.h"
+
+enum {
+    CHUNK_SIZE = 16,
+    CHUNK_SIZE_DEPTH = 10
+};
+
 typedef bool Block;
+
+typedef struct {
+    Block blocks[CHUNK_SIZE_DEPTH][CHUNK_SIZE][CHUNK_SIZE];
+} Chunk;
+
+typedef void (*RenderChunkCallback)(const Coords3D*, const Block*) ;
+
+
+Chunk generateChunk(void);
+
+void renderChunk(const Chunk* chunk, const RenderChunkCallback renderCallback);
---
+++ src/world.c
@@ -0,0 +1,31 @@
+#include "world.h"
+#include <time.h>
+#include <stdlib.h>
+
+Chunk generateChunk(void) {
+    Chunk chunk;
+    srand(time(NULL));
+
+    for(int i = 0; i < CHUNK_SIZE_DEPTH; i++) {
+        for(int j = 0; j < CHUNK_SIZE; j++) {
+            for(int k = 0; k < CHUNK_SIZE; k++) {
+                chunk.blocks[i][j][k] = rand() % 2;
+            }
+        }
+    }
+
+    return chunk;
+}
+
+
+void renderChunk(const Chunk* chunk, const RenderChunkCallback renderCallback) {
+    for(int i = 0; i < CHUNK_SIZE_DEPTH; i++) {
+        for(int j = 0; j < CHUNK_SIZE; j++) {
+            for(int k = 0; k < CHUNK_SIZE; k++) {
+                const Block* block = &chunk->blocks[i][j][k];
+                const Coords3D coords = { k, i, j };
+                renderCallback(&coords, block);
+            }
+        }
+    }
+}
